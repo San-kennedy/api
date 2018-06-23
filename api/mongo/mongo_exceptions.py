c50c4a191717fa90a0fde35269a25163e1ac4164
San-kennedy <San-kennedy@users.noreply.github.com>
2018-06-23 14:36:50 +0530

    fixing comments
    fixing duplicate comments due to ctrl-C, ctrl-V

--- api/mongo/mongo_exceptions.py
+++ api/mongo/mongo_exceptions.py
@@ -10,16 +10,16 @@ class DbError(Exception):
         Exception.__init__(self,msg)
 
 class CollectionError(Exception):
-    """exception when DB does not exist"""
+    """exception when Collection does not exist"""
     def __init__(self,msg):
         Exception.__init__(self,msg)
 
 class ContentTypeUnsupported(Exception):
-    """exception when DB does not exist"""
+    """exception when Unsupported content type is posted"""
     def __init__(self):
         Exception.__init__(self, "Unsupported content type. Supported application JSON")
 
 class IllegalArgumentException(Exception):
-    """exception when DB does not exist"""
+    """exception when Arguments in JSON do not meet standards"""
     def __init__(self):
         Exception.__init__(self, "illegal_argument_exception")
